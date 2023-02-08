2336099dfb0bca3ac4710dbc1e668281fce6a3a5
이재열/RE팀 <jyeory@gsretail.com>
2023-02-08 14:55:11 +0900

    udemy - 1주차 - 로그인 페이지 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,45 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          title: Text(
+            'Hello Flutter',
+            style: TextStyle(fontSize: 28),
+          ),
+          centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+          // SingleChildScorllView 한 손가락 스크롤 뷰
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: Image.network(
+                    "https://i.ibb.co/CwzHq4z/trans-logo-512.png",
+                    width: 81,
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: '이메일'),
+                ),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(labelText: '비밀번호'),
+                ),
+                Container(
+                  width: double.infinity, // 기기에 관계 없이 꽉 채우기
+                  margin: EdgeInsets.only(top: 24),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text('LOGIN'),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
